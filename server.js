b555075393bb89fa19447cf17412f8d519436892
Nikkie Mashian <nikkiemashian@gmail.com>
2018-12-31 16:34:01 -0800

    minimal error handling

--- server.js
+++ server.js
@@ -12,88 +12,110 @@ app.use(
   })
 );
 
-app.get('/api/pawfiles', (req, res) => {
-  res.json({
-    user: {firstName: 'Nikkie', lastName: 'Mashian'},
-    sortingPetsMethod: '',
-    showPawfileForm: false,
-    showMedicalForm: false,
-    showMemoryForm: false,
-    currentPetId: undefined,
-    currentSearchTerm: '',
-    categoryFilter: '',
-    toggleNavbar:false,
-    pawfiles: [
+let pawfiles =[
+  {
+    id: 0,
+    name: 'Mushy',
+    species: 'Cat',
+    gender: 'Female',
+    breed: 'Domestic Mix',
+    weight: '8 lbs',
+    birthday: '2016-10-26',
+    bio: 'Meow. I\'m a cute troublemaker. I\'ll purr then hiss. Give me scritches?',
+    img: 'https://i.ibb.co/y8hFnkL/2.jpg',
+    reminders: [
       {
         id: 0,
-        name: 'Mushy',
-        species: 'Cat',
-        gender: 'Female',
-        breed: 'Domestic Mix',
-        weight: '8 lbs',
-        birthday: '2016-10-26',
-        bio: 'Meow. I\'m a cute troublemaker. I\'ll purr then hiss. Give me scritches?',
-        img: 'https://i.ibb.co/y8hFnkL/2.jpg',
-        reminders: [
-          {
-            id: 0,
-            note: 'Trim Nails',
-            date: '2016-10-26',
-          },
-          {
-            id: 1,
-            note: 'Vet Appointment',
-            date: '2016-11-26',
-          },
-        ],
-        posts: [
-          {
-            id: 0,
-            type: 'memory',
-            title: 'Mushy learns how to open the door',
-            date: 'Fri Dec 14 2018',
-            description: 'I walked into the living room and saw her opening it with her claws. How dare she!',
-            memory_img: 'https://i.ibb.co/YXHrzCq/Screen-Shot-2018-12-31-at-8-30-37-AM.png" alt="Screen-Shot-2018-12-31-at-8-30-37-AM'
-          },
-          {
-            id: 1,
-            type: 'medical',
-            title: 'Shes throwing up again:(',
-            date: 'Fri Dec 14 2017',
-            symptoms: ['lethargic', 'no appetite'],
-            vaccinations:['rabies'],
-            prescriptions:['Frontline flea'],
-            doctor: 'Dr. Moon',
-            notes: 'Gave her fluids for the day. Wont let her eat until tomorrow. Try laxatives.',
-          }
-        ],
-        vaccinations: [
-          {
-            name: 'Rabies',
-            date: '2018-10-12'
-          }
-        ],
-        prescriptions:[]
+        note: 'Trim Nails',
+        date: '2016-10-26',
       },
       {
         id: 1,
-        name: 'Muffin',
-        species: 'Dog',
-        gender: 'Male',
-        breed: 'Pom/Yorkie Mix',
-        birthday: '2010-01-10',
-        bio: 'Ruff. I want to always play and go on walks. Did you say snack?',
-        img: 'https://i.ibb.co/stMyFMp/IMG-6267.png',
-        reminders: [
-          {
-            id: 0,
-            note: 'Give Shot',
-            date: 'Daily'
-          },
-        ]
-      }, 
+        note: 'Vet Appointment',
+        date: '2016-11-26',
+      },
+    ],
+    posts: [
+      {
+        id: 0,
+        type: 'memory',
+        title: 'Mushy learns how to open the door',
+        date: 'Fri Dec 14 2018',
+        description: 'I walked into the living room and saw her opening it with her claws. How dare she!',
+        memory_img: 'https://i.ibb.co/YXHrzCq/Screen-Shot-2018-12-31-at-8-30-37-AM.png" alt="Screen-Shot-2018-12-31-at-8-30-37-AM'
+      },
+      {
+        id: 1,
+        type: 'medical',
+        title: 'Shes throwing up again:(',
+        date: 'Fri Dec 14 2017',
+        symptoms: ['lethargic', 'no appetite'],
+        vaccinations:['rabies'],
+        prescriptions:['Frontline flea'],
+        doctor: 'Dr. Moon',
+        notes: 'Gave her fluids for the day. Wont let her eat until tomorrow. Try laxatives.',
+      }
+    ],
+    vaccinations: [
+      {
+        name: 'Rabies',
+        date: '2018-10-12'
+      }
+    ],
+    prescriptions:[]
+  },
+  {
+    id: 1,
+    name: 'Muffin',
+    species: 'Dog',
+    gender: 'Male',
+    breed: 'Pom/Yorkie Mix',
+    birthday: '2010-01-10',
+    bio: 'Ruff. I want to always play and go on walks. Did you say snack?',
+    img: 'https://i.ibb.co/stMyFMp/IMG-6267.png',
+    reminders: [
+      {
+        id: 0,
+        note: 'Give Shot',
+        date: 'Daily'
+      },
     ]
+  }, 
+];
+
+app.get('/api/pawfiles', (req, res) => {
+  res.json({
+    pawfiles: pawfiles
   });
+});
+
+app.get('/api/:pawfileId', (req, res, next) => {
+  
+  let pawfile = pawfiles.find(pawfile=>req.params.pawfileId==pawfile.id);
+
+  console.log('in server, pawfile is', pawfile);
+  if(pawfile){
+    console.log('sending back response');
+    res.json({
+      pawfile: pawfile
+    });
+  }
+  else{
+    console.log('jumping to error');
+    next();
+  }
+
+});
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 app.listen(8080);
