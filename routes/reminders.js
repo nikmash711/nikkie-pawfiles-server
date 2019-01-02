4ca4628cd21791b0be53885d64762e10a565dff4
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-02 09:07:20 -0800

    deleting reminder works

--- routes/reminders.js
+++ routes/reminders.js
@@ -28,4 +28,32 @@ router.post('/:pawfileId', (req, res, next) => {
     });
 });
 
+/* DELETE REMINDER */
+router.delete('/:pawfileId/:reminderId', (req, res, next) => {
+  const { pawfileId, reminderId } = req.params;
+
+  //remove the reminder
+  const reminderRemovePromise = Reminder.findOneAndDelete({_id:reminderId});
+
+  // Don't delete the pawfile associated with the reminder to be deleted, but just remove the reminder from the reminder array
+  const pawfileReminderPullPromise = Pawfile.findByIdAndUpdate(pawfileId,
+    { $pull: { reminders: reminderId } }
+  );
+
+  // delete the reminder and update the pawfile in parallel using .all
+  Promise.all([reminderRemovePromise, pawfileReminderPullPromise])
+    .then((reminder) => {
+      // We want to make sure that it doesn't try to delete a reminder that no longer exists or never existed. To prevent that, we need to check this: 
+      if(reminder[0]!==null){
+        res.status(204).end();
+      }
+      else{
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
